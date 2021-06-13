8de2d8dfbcd886ff04e4cde7425b6842ab54f9f1
somer1221 <79031075+somer1221@users.noreply.github.com>
2021-06-13 11:59:46 -0400

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -12,5 +12,29 @@ function addUser() {
 }
 
 function send() {
-    
+
+    player1_score = 0
+    player2_score = 0
+
+    document.getElementById("player1_name").innerHTML = player1_name + ":";
+    document.getElementById("player2_name").innerHTML = player2_name + ":";
+
+    document.getElementById("player1_score").innerHTML = player1_score;
+    document.getElementById("player2_score").innerHTML = player2_score;
+
+    document.getElementById("player_question").innerHTML = player1_name + "'s Turn To Give A Question";
+    document.getElementById("player_answer").innerHTML = player2_name + "'s Turn To Answer";
+
+    number1 = document.getElementById("1st_number").value;
+    number2 = document.getElementById("2nd_number").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+    question_number = "<h4>" + number1 + "x" + number2 + "</h4>";
+    answer_input = "<br> Answer : <input type='text' id='answer_input'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_number + answer_input + check_button;
+
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("1st_number").value = "";
+    document.getElementById("2nd_number").value = "";
 }
